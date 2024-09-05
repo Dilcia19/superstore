701b928bee85a43a71897611e7e61a68246a27d0
Dilcia Mercedes <dilcia.mercedes@paramount.com>
2024-09-05 17:22:44 -0400

    table created in db

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -1,6 +1,24 @@
 CREATE TABLE store_records (
     id SERIAL PRIMARY KEY,
-    Row ID INT,
-    Order ID INT,
-    Ship Date VARCHAR(10)
+    row_id INT,
+    order_id INT,
+    order_date VARCHAR(10), 
+    ship_date VARCHAR(10),
+    ship_mode VARCHAR(25),
+    customer_id VARCHAR(20),
+    customer_name VARCHAR(50),
+    segment VARCHAR(25),
+    country VARCHAR(25),
+    city VARCHAR(30),
+    state VARCHAR(25),
+    postal_code INT,
+    region VARCHAR(25),
+    product_id VARCHAR(30),
+    category VARCHAR(30)
+    subcategory VARCHAR(30),
+    product_name VARCHAR(70),
+    sales FLOAT,
+    quantity INT,
+    discount FLOAT,
+    profit FLOAT
 );
