1e63979026b1ba9661546265757135e2dcc286f9
Dilcia Mercedes <dilcia.mercedes@paramount.com>
2024-09-05 17:15:34 -0400

    updated data exported

--- scripts/clean_headers.py
+++ scripts/clean_headers.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-superstore = pd.read_csv('../data/sample_superstore.csv')
+superstore = pd.read_csv('../data/sample_superstore.csv', encoding='ISO-8859-1')
 
-superstore.columns = superstore.columns.str.reaplace(' ','_')
+superstore.columns = superstore.columns.str.replace(' ','_')
 superstore.columns = superstore.columns.str.lower()
 
 superstore.to_csv('../data/sample_superstore_updated.csv', index=False)
