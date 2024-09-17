810c476e2f8a7c6dd2a5d0c42dfe8bd053b2d8c0
Dilcia Mercedes <dilcia.mercedes@paramount.com>
2024-09-16 20:55:53 -0400

    added year col

--- scripts/clean_headers.py
+++ scripts/clean_headers.py
@@ -5,4 +5,6 @@ superstore = pd.read_csv('data/sample_superstore.csv', encoding='ISO-8859-1')
 superstore.columns = superstore.columns.str.replace(' ','_').str.replace('-','_')
 superstore.columns = superstore.columns.str.lower()
 
+superstore['year'] = superstore['order_date'].str[-4:]
+
 superstore.to_csv('data/sample_superstore_updated.csv', index=False)
