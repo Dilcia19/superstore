91368deb83263184b58cb08b98e6eafc2c796189
Dilcia Mercedes <dilcia.mercedes@paramount.com>
2024-10-20 16:46:56 -0400

    analysis notebook removed, sql scripts removed

--- scripts/insert_records.sql
+++
@@ -1,6 +0,0 @@
-COPY store_records(row_id, order_id, order_date, ship_date, ship_mode,
-       customer_id, customer_name, segment, country, city, state,
-       postal_code, region, product_id, category, sub_category,
-       product_name, sales, quantity, discount, profit)
-       FROM '/Users/Filepath/sample_superstore_updated.csv' DELIMITER ',' CSV HEADER;
-
